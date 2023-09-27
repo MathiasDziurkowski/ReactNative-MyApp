e74ccb0d6072a0885c10aad519614d8ae13cfb73
Mathias Dziurkowski <128103391+MathiasDziurkowski@users.noreply.github.com>
2023-09-27 16:44:10 -0300

    v1.1

--- src/Jogo.js
+++ src/Jogo.js
@@ -1,107 +1,51 @@
 import { useState } from "react";
-import { Stylesheet, View, Text, Button, StyleSheet, FlatList } from "react-native";
+import { Stylesheet, View, Text, Button, StyleSheet, FlatList, Pressable } from "react-native";
 
 export default function Jogo(props) {
+    const [matriz, setMatriz] = useState(
+        [
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]
+    )
+    const[vezJogador,setVezJogador] = useState("X")
+
+    function VezJogador() {
+        if (vezJogador == "X"){
+            setVezJogador("O")
+        }
+        else {
+            setVezJogador("X")
+        }
+    }
+
+    const handleClickB = () => {
+        VezJogador()
+    }
+
     const handleClick = (event) => {
-        props.changeScreen("home"); 
-    }
-
-    const [b1, setB1] = useState("");
-    const [b2, setB2] = useState("");
-    const [b3, setB3] = useState("");
-    const [b4, setB4] = useState("");
-    const [b5, setB5] = useState("");
-    const [b6, setB6] = useState("");
-    const [b7, setB7] = useState("");
-    const [b8, setB8] = useState("");
-    const [b9, setB9] = useState("");
-
-    const [vez,setVez]  = useState("X");
-
-    const trocarVez = () => {
-        if (vez == "X"){
-            setVez("O")
-        }
-        else{
-            setVez("X")
-        }
-    }
-
-    const handleClickB1 = () => {
-        if (b1 == ""){
-            setB1(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB2 = () => {
-        if (b2 == ""){
-            setB2(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB3 = () => {
-        if (b3 == ""){
-            setB3(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB4 = () => {
-        if (b4 == ""){
-            setB4(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB5 = () => {
-        if (b5 == ""){
-            setB5(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB6 = () => {
-        if (b6 == ""){
-            setB6(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB7 = () => {
-        if (b7 == ""){
-            setB7(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB8 = () => {
-        if (b8 == ""){
-            setB8(vez)
-            trocarVez()
-        }
-    }
-
-    const handleClickB9 = () => {
-        if (b9 == ""){
-            setB9(vez)
-            trocarVez()
-        }
+        props.changeScreen("home");
     }
     return (
         <View >
             <Text>Jogo</Text>
             <Button title='Voltar' onPress={handleClick}></Button>
-            <Button title={b1} onPress={handleClickB1}></Button>
-            <Button title={b2} onPress={handleClickB2}></Button>
-            <Button title={b3} onPress={handleClickB3}></Button>
-            <Button title={b4} onPress={handleClickB4}></Button>
-            <Button title={b5} onPress={handleClickB5}></Button>
-            <Button title={b6} onPress={handleClickB6}></Button>
-            <Button title={b7} onPress={handleClickB7}></Button>
-            <Button title={b8} onPress={handleClickB8}></Button>
-            <Button title={b9} onPress={handleClickB9}></Button>
+            <View>
+                <Pressable onPress={handleClickB}><Text>{matriz[0][0]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[0][1]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[0][2]}</Text></Pressable>
+            </View>
+            <View>
+                <Pressable onPress={handleClickB}><Text>{matriz[1][0]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[1][1]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[1][2]}</Text></Pressable>
+            </View>
+            <View>
+                <Pressable onPress={handleClickB}><Text>{matriz[2][0]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[2][1]}</Text></Pressable>
+                <Pressable onPress={handleClickB}><Text>{matriz[2][2]}</Text></Pressable>
+            </View>
         </View>
 
     )
@@ -109,7 +53,7 @@ export default function Jogo(props) {
 
 const styles = StyleSheet.create({
     view: {
-        
+
     },
 
 
